357066c4fb075648f23e58c51f4f42c50db74811
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-12-16 00:13:31 +0000

    Update wrapper.h to use the overall header

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,1 @@
-#include "common/RabbitizerConfig.h"
-#include "instructions/RabbitizerInstruction.h"
-#include "instructions/RabbitizerInstructionRsp.h"
-#include "instructions/RabbitizerInstructionR5900.h"
-#include "instructions/RabbitizerRegister.h"
-#include "instructions/RabbitizerInstrSuffix.h"
+#include "rabbitizer.h"
